36ba7493e957b57e89069517266ac564dbfedca7
muh-younus <braq369@gmail.com>
2024-12-09 11:55:49 +0500

    another commit

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 let featureButton = document.querySelector(".featureBtn");
 let featureShow = document.querySelector(".feat-show");
 
-let count=1;
+let serviceButton = document.querySelector(".serviceBtn");
+let serviceShow = document.querySelector(".service-show");
+
+let barButton = document.querySelector(".list-button");
+let sideBar = document.querySelector(".sider-bar");
 
 
+barButton.addEventListener("click", () => {
+
+    barButton.classList.toggle("click");
+    sideBar.classList.toggle("show");
+})
+
+
+// Service-Button-section
+
+serviceButton.addEventListener("click" ,() => {
+
+    
+    serviceShow.classList.toggle("service");
+
+    
+});
+featureButton.addEventListener("click" ,() => {
+
+    
+    featureShow.classList.toggle("feature");
+
+    
+});
+
+
+// Feature-Button-section
 featureButton.addEventListener("click", () => {
     
    
-    if(count == 1){
+    if(count == true){
 
         featureShow.style = "display: block";
 
-        count=0;
+        count=false;
 
 
     }
     else{
 
         featureShow.style = "display: none";
-        count=1;
+        count=true;
     }
 
-   
-
-   
-
-
-})
+});
 
